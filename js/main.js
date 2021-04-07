c397609fa273db11847a61883c5a1423798fa766
Erik Olsen <eo.olsen@gmail.com>
2021-04-07 17:14:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 $(document).ready( function() {
 
-	// Logo
-	var $logo 	= $('#logo');
-	 if (location.href.indexOf("#") != -1) {
-        if(location.href.substr(location.href.indexOf("#"))!='#about'){
-        	$logo.show();
-        }
-    }
-    
-	// Show logo 
-	$('#tab-container .tab a').click(function() {
-	  $logo.slideDown('slow');
-	});
-	// Hide logo
-	$('#tab-about').click(function() {
-	  $logo.slideUp('slow');
-	});	
 function animMeter(){
     $(".meter > span").each(function() {
                 $(this)
